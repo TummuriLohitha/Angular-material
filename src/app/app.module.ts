83f80275594a6830226686f9d99570341763b9bf
TummuriLohitha <36749792+TummuriLohitha@users.noreply.github.com>
2018-04-03 18:11:48 -0500

    Angular material

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { UsertableComponent } from './components/usertable/usertable.component';
+import { MatTableModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { UserService } from './services/user.service';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsertableComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    HttpClientModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,9 @@
+export interface User {
+    name: string;
+    username: string
+    email: string;
+    phone: string;
+    company: {
+        name: string;
+    }
+}
